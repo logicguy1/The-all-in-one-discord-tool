28fe4c081b242ee9f8f872d340d86120cac093f9
logicguy1 <56993729+logicguy1@users.noreply.github.com>
2021-03-23 15:08:32 +0100

    Update credits.py

--- modules/credits.py
+++ modules/credits.py
@@ -7,7 +7,7 @@ w = fg(15)
 
 def show_credits():
     print(f"\n {r2}[{b}+{r2}] Author: Drillenissen#4268")
-    print(f" {r2}[{b}+{r2}] Discord Server: https://discord.com/invite/anonymous")
+    print(f" {r2}[{b}+{r2}] Discord Server: https://discord.gg/TMshrBeyHE")
     print(f" {r2}[{b}+{r2}] Github: https://github.com/logicguy1")
 
 def show_help():
